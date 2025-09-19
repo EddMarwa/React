2da5553dd8a0e2e729c7cc8b1d3825a598cc7eca
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-20 02:50:09 +0300

    update

---
+++ tutorial.js
@@ -0,0 +1,15 @@
+function DoSomething() {
+
+}
+
+const DoSomething = () => {
+
+}
+
+const person = {
+    name: "Alice",
+    age: 30,
+    greet: function() {
+        console.log("Hello, " + this.name);
+    }
+}
