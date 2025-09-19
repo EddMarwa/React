b64c61d6e928f12976777bed37c9c0947d8a57ce
Edd <149029644+EddMarwa@users.noreply.github.com>
2025-09-20 02:30:12 +0300

    update

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+        <h1>My React App</h1>
+        <nav>
+            <ul>
+                <li><a href="#"></a>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Blog</li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+export default Header;
